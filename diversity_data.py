63da8013ad93fec22f825f1405767062917f16e1
Sebastiaan Kruize <sebaskruize@gmail.com>
2020-09-22 14:23:29 +0200

    last bit of data

--- diversity_data.py
+++ diversity_data.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
-def plotdiversity(enemy, method):
+def plotdiversityislands(enemy, method):
     """select enemy from 1-3, method = 'random' or 'best'"""
 
     # make dicts to save the data per gen for diversity, mean and std
@@ -50,7 +50,7 @@ def plotdiversity(enemy, method):
         y_error.append(y_err)
 
 
-    fig, ax = plt.subplots()
+    #fig, ax = plt.subplots()
 
 
     ax.errorbar(x_data, y_data,
@@ -67,6 +67,73 @@ def plotdiversity(enemy, method):
 
     # plt.show()
 
-plotdiversity(1, 'random')
-plotdiversity(1, 'best')
+
+def plotdiversity_no_islands(enemy):
+    """plots diversity data for no islands, just the base model"""
+    # make dicts to save the data per gen for diversity, mean and std
+    data_mean = {}
+    data_std ={}
+
+    for i in range(0, 10):
+
+        with open(f'En{enemy}_no_isl_{i}/diversity.csv') as csv_file:
+            reader = csv.reader(csv_file, delimiter=' ')
+            current_row = 1
+
+            div = []
+            div_all_gens = []
+            std_all_gens = []
+            for row in reader:
+                if current_row > 1:
+                    div.append(int(row[1]))
+
+                if (current_row != 1) and ((current_row-1) % 1 == 0):
+                    
+                    div_all_gens.append(np.mean(div))
+                    std_all_gens.append(np.mean(div))
+                    div = []
+                current_row += 1
+
+            data_mean[f'run{i}'] = div_all_gens
+            data_std[f'run{i}'] = std_all_gens
+
+    # now we get the actual lists to plot the data by taking the averages of the averages and std's
+
+    y_data = []
+    y_error = []
+    x_data = np.linspace(1,20,19)
+    #print(len(data_mean['run0']))
+    #print(data_mean)
+    print(data_std)
+    for i in range(0, 19):
+        y = np.mean([data_mean['run0'][i], data_mean['run1'][i], data_mean['run2'][i], data_mean['run3'][i], \
+            data_mean['run4'][i], data_mean['run5'][i], data_mean['run6'][i], data_mean['run7'][i], data_mean['run8'][i], data_mean['run9'][i] ])
+        y_data.append(y)
+        y_err = np.std([data_std['run0'][i], data_std['run1'][i], data_std['run2'][i], data_std['run3'][i], data_std['run4'][i], \
+            data_std['run5'][i], data_std['run6'][i], data_std['run7'][i], data_std['run8'][i], data_std['run9'][i] ])
+        y_error.append(y_err)
+
+
+    #fig, ax = plt.subplots()
+
+
+    ax.errorbar(x_data, y_data,
+                yerr=y_error,
+                capsize=5,
+                fmt='-o')
+
+
+    ax.set_xlabel('Generation')
+    ax.set_ylim(0, 30)
+    ax.set_ylabel('Diversity')
+    #ax.set_title('Enemy 1')
+
+
+    # plt.show()
+
+
+fig, ax = plt.subplots()
+#plotdiversityislands(3, 'random')
+plotdiversityislands(3, 'best')
+plotdiversity_no_islands(3)
 plt.show()
